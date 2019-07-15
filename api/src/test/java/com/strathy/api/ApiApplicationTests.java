fa414a6ff959521cb1d72859660961c36fb3e5c1
Ravjot <ravjot28@gmail.com>
2019-07-14 23:03:41 -0400

    Updated the travis config and java api app to run java test

--- api/src/test/java/com/strathy/api/ApiApplicationTests.java
+++ api/src/test/java/com/strathy/api/ApiApplicationTests.java
@@ -2,11 +2,9 @@ package com.strathy.api;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
 public class ApiApplicationTests {
 
   @Test
