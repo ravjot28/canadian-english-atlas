ca71ac603c78fa545a24d55d48aba9f469749d49
Ravjot Singh <ravjotsingh@ravjots-mbp.lan>
2019-07-28 19:09:38 -0400

    Added a default  value of api path if env is not present

--- client/src/common/constants/index.js
+++ client/src/common/constants/index.js
@@ -1,4 +1,7 @@
-export const API_BASE_URL = process.env.REACT_APP_SERVER_URL;
+export const API_BASE_URL =
+  process.env.REACT_APP_SERVER_URL === undefined
+    ? "/api"
+    : process.env.REACT_APP_SERVER_URL;
 //export const API_BASE_URL = "/api";
 export const ACCESS_TOKEN = "accessToken";
 
